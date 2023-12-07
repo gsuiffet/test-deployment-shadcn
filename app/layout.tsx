26f629124323f526bcc958a1f24682b05608a0c9
Guillaume <suiffet.g@gmail.com>
2023-12-07 15:22:15 +0100

    test88

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,62 @@
-import '@/app/globals.css'
-import { cn } from '@/lib/utils'
-import { Header } from '@/components/header'
-import React from "react";
-interface RootLayoutProps {
-  children: React.ReactNode
-}
+import React from 'react';
+import type { Metadata } from 'next';
+import { Inter as FontSans } from 'next/font/google';
+import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
+import { cn } from '@/lib/utils';
+import { Header } from '@/components/header';
+import { ToastContainer } from 'react-toastify';
+import { Menu } from '@/components/menu';
+import Link from 'next/link';
 
-export default function RootLayout({ children }: RootLayoutProps) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
-        <div className="flex flex-col min-h-screen">
-          <Header />
-          <main className="flex flex-col flex-1 bg-muted/50">{children}</main>
-        </div>
-      </body>
-    </html>
-  )
+const fontSans = FontSans({
+    subsets: ['latin'],
+    variable: '--font-sans',
+});
+
+export const metadata: Metadata = {
+    title: 'Tokens Studio Tailwind Documentation',
+    description: 'Comprehensive documentation for Tokens Studio Tailwind package',
+    authors: {
+        name: 'Suiffet Guillaume',
+    },
+    keywords: ['Tokens Studio', 'Tailwind', 'Documentation', 'Design Tokens', 'CSS Variables'],
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html suppressHydrationWarning={true}>
+        <body lang="en" className={cn('bg-background font-sans antialiased', fontSans.variable)}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            <div className="flex flex-col min-h-screen">
+                <Header /> 88
+                <div className="container flex">
+                    <aside className="hidden md:flex w-60">
+                        <Menu />
+                    </aside>
+                    <main>
+                        <div className="w-full md:max-w-[550px] lg:max-w-[980px]">{children}</div>
+                    </main>
+                </div>
+                <footer className="container mt-auto h-14 border-t flex items-center bg-background">
+                    <p className="text-muted-foreground text-xs sm:text-sm">
+                        This website has been made using <strong>Tokens Studio Tailwind</strong>. See the
+                        source code on {/*todo*/}
+                        <Link href="https://www" className="underline">
+                            GitHub
+                        </Link>
+                        .
+                    </p>
+                </footer>
+            </div>
+            <ToastContainer closeButton={false} autoClose={1500} />
+        </ThemeProvider>
+        </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,44 @@
-import { nanoid } from '@/lib/utils'
-import { Chat } from '@/components/chat'
-import {Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger} from "@/components/ui/sheet";
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import React from 'react';
+import diagram from './lotties/diagram.json';
+import { Icon } from '@/components/icon';
+import { Next } from '@/components/next';
+import { PageHeader } from '@/components/pageHeader';
+import { Lottie } from '@/components/lottie';
 
-export default function IndexPage() {
-  const id = nanoid()
+export default function Home() {
+  const description = (
+      <>
+        <strong>Open source project</strong> that converts design tokens from{' '}
+        <Link href="https://tokens.studio" target="_blank" className="underline">
+          Tokens Studio
+        </Link>{' '}
+        into readily accessible Tailwind CSS variables and components.
+      </>
+  );
 
-  return <>
-    <Sheet>
-      <SheetTrigger>Open</SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Are you sure absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your account
-            and remove your data from our servers.
-          </SheetDescription>
-        </SheetHeader>
-      </SheetContent>
-    </Sheet>
-    <Chat id={id} />
-  </>
-
+  return (
+      <>
+        <PageHeader title="Supercharge Tailwind with design tokens" description={description} />
+        <div className="flex w-full items-center px-4 space-x-4 pb-8 pt-4 md:pb-10">
+          <Link href="/introduction">
+            <Button>Get Started</Button>
+          </Link>
+          <Link href="https://github.com/gsuiffet/tokens-studio-tailwind" target="_blank">
+            <Button variant="outline" className="flex space-x-3 items-center">
+              <Icon name="github" size="large" />
+              <span>GitHub</span>
+            </Button>
+          </Link>
+        </div>
+        <div className="flex justify-center">
+          <Lottie
+              animationData={diagram}
+              className="w-[80vw] md:w-full max-w-[600px] bg-white rounded-md"
+          />
+        </div>
+        <Next nextStep="introduction" />
+      </>
+  );
 }
