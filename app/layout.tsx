ced086e5dc0f49ddd57279cc6833c7813b73c339
Guillaume <suiffet.g@gmail.com>
2023-12-07 14:49:55 +0100

    test5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,3 @@
-import { Toaster } from 'react-hot-toast'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-
 import '@/app/globals.css'
 import { cn } from '@/lib/utils'
 import { Header } from '@/components/header'
@@ -13,14 +9,7 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          'font-sans antialiased',
-          GeistSans.variable,
-          GeistMono.variable
-        )}
-      >
-        <Toaster />
+      <body>
         <div className="flex flex-col min-h-screen">
           <Header />
           <main className="flex flex-col flex-1 bg-muted/50">{children}</main>
