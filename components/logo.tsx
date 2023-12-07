61d386888fdc150a59ed40ce95eb221aa30e10c0
Guillaume <suiffet.g@gmail.com>
2023-12-07 14:23:48 +0100

    test3

--- components/logo.tsx
+++ components/logo.tsx
@@ -8,16 +8,15 @@ interface Props {
 }
 
 export function Logo ({ classname }: Props) {
-    return <></>
-  // return (
-  //   <Link href="/" className={`space-x-2 items-center h-full ${classname}`}>
-  //     <div className="pt-1 h-12 w-12 relative">
-  //       <Image src="/logo.svg" alt="logo" fill className="scale-100 dark:scale-0" />
-  //       <Image src="/logo-negative.svg" alt="logo" fill className="scale-0 dark:scale-100" />
-  //     </div>
-  //     <p className="text-accent-foreground font-bold inline-block text-sm uppercase">
-  //       tokens studio tailwind
-  //     </p>
-  //   </Link>
-  // );
+  return (
+    <Link href="/" className={`space-x-2 items-center h-full ${classname}`}>
+      <div className="pt-1 h-12 w-12 relative">
+        <Image src="/logo.svg" alt="logo" fill className="scale-100 dark:scale-0" />
+        <Image src="/logo-negative.svg" alt="logo" fill className="scale-0 dark:scale-100" />
+      </div>
+      <p className="text-accent-foreground font-bold inline-block text-sm uppercase">
+        tokens studio tailwind
+      </p>
+    </Link>
+  );
 }
