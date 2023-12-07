305ba34ac49c9198366dd04d86121a5bbb9bfef6
Guillaume <suiffet.g@gmail.com>
2023-12-07 14:35:52 +0100

    test4

--- components/header.tsx
+++ components/header.tsx
@@ -9,7 +9,7 @@ export function Header() {
 
     return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-full px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
-        <Logo classname="hidden md:flex" /> Header
+        <Logo classname="hidden md:flex" /> Header22
         <Chat id={id} />
         <TailwindIndicator />
     </header>
