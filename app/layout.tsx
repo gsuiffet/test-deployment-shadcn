46bd778137403165fa94d9858ca4677d285b42fe
Guillaume <suiffet.g@gmail.com>
2023-12-07 15:25:14 +0100

    test99

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             disableTransitionOnChange
         >
             <div className="flex flex-col min-h-screen">
-                <Header /> 88
+                <Header /> 99
                 <div className="container flex">
                     <aside className="hidden md:flex w-60">
                         <Menu />
--- components/header.tsx
+++ components/header.tsx
@@ -1,17 +1,42 @@
-import * as React from 'react'
+import React, { FC } from 'react';
+import Link from 'next/link';
 import { Logo } from '@/components/logo';
-import {Chat} from "@/components/chat";
-import { TailwindIndicator } from '@/components/tailwind-indicator'
-import {nanoid} from "@/lib/utils";
+import { ModeToggle } from '@/components/mode-toggle';
+import { Button } from '@/components/ui/button';
+// import { Drawer } from '@/components/drawer';
+import { Icon } from '@/components/icon';
 
-export function Header() {
-    const id = nanoid()
-
+const Header: FC = () => {
     return (
-    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-full px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
-        <Logo classname="hidden md:flex" /> Header77
-        <Chat id={id} />
-        <TailwindIndicator />
-    </header>
-  )
-}
+        <header className="container h-14 sticky top-0 z-50 border-b bg-background/95 backdrop-blur">
+            <div className="flex justify-between items-center h-full w-full px-4 md:px-16">
+                <div className="md:hidden">
+                    {/*<Drawer />*/}
+                </div>
+                <Logo classname="hidden md:flex" />
+                <div className="flex items-center">
+                    <nav className="hidden md:flex mr-5" role="navigation" aria-label="Main Navigation">
+                        <Link href="/introduction" className="ml-5 main-navigation-full">
+                            documentation
+                        </Link>
+                        <Link href="/primitives" className="ml-5 main-navigation-full">
+                            examples
+                        </Link>
+                    </nav>
+                    <ModeToggle />
+                    <Link
+                        href="https://github.com/gsuiffet/tokens-studio-tailwind"
+                        target="_blank"
+                        className="ml-5 flex items-center"
+                    >
+                        <Button variant="outline" size="icon">
+                            <Icon name="github" size="large" />
+                        </Button>
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export { Header };
