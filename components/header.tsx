177ee9a231325a76d2f31db7d64ae5071d6ba8bd
Guillaume <suiffet.g@gmail.com>
2023-12-07 14:21:04 +0100

    test2

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react'
+import { Logo } from '@/components/logo';
+import {Chat} from "@/components/chat";
+import { TailwindIndicator } from '@/components/tailwind-indicator'
+import {nanoid} from "@/lib/utils";
 
 export function Header() {
-  return (
-    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
-      Header
+    const id = nanoid()
+
+    return (
+    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-full px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
+        {/*<Logo classname="hidden md:flex" /> Header*/}
+        <Chat id={id} />
+        <TailwindIndicator />
     </header>
   )
 }
