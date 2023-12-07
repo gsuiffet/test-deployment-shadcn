06af6c0b270a53fae24437798ff9bb5185565a4f
Guillaume <suiffet.g@gmail.com>
2023-12-07 16:10:35 +0100

    108

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,40 +23,44 @@ export const metadata: Metadata = {
   keywords: ['Tokens Studio', 'Tailwind', 'Documentation', 'Design Tokens', 'CSS Variables'],
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html suppressHydrationWarning={true}>
+      <html suppressHydrationWarning={true}>
       <body lang="en" className={cn('bg-background font-sans antialiased', fontSans.variable)}>
-        <ThemeProvider
+      <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
-        >
-          <div className="flex flex-col min-h-screen">
-            <Header /> 104
-            <div className="container flex">
-              <aside className="hidden md:flex w-60">
-                <Menu />
-              </aside>
-              <main>
-                <div className="w-full md:max-w-[550px] lg:max-w-[980px]">{children}</div>
-              </main>
-            </div>
-            <footer className="container mt-auto h-14 border-t flex items-center bg-background">
-              <p className="text-muted-foreground text-xs sm:text-sm">
-                This website has been made using <strong>Tokens Studio Tailwind</strong>. See the
-                source code on {/*todo*/}
-                <Link href="https://www" className="underline">
-                  GitHub
-                </Link>
-                .
-              </p>
-            </footer>
+      >
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <div className="container flex">
+            <aside className="hidden md:flex w-60">
+              <Menu />
+            </aside>
+            <main>
+              <div className="w-full md:max-w-[550px] lg:max-w-[980px]">{children}</div>
+            </main>
           </div>
-          <ToastContainer closeButton={false} autoClose={1500} />
-        </ThemeProvider>
+          <footer className="container mt-auto h-14 border-t flex items-center bg-background">
+            <p className="text-muted-foreground text-xs sm:text-sm">
+              This website has been made using <strong>Tokens Studio Tailwind</strong>. See the
+              source code on {/*todo*/}
+              <Link href="https://www" className="underline">
+                GitHub
+              </Link>
+              .
+            </p>
+          </footer>
+        </div>
+        <ToastContainer closeButton={false} autoClose={1500} />
+      </ThemeProvider>
       </body>
-    </html>
+      </html>
   );
 }
