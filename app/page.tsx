ef34c2d09ad46a2260467c669f3518c625197e7d
Guillaume Suiffet <guillaumesuiffet@MacBook-Pro-de-Guillaume.local>
2023-12-07 13:38:16 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
 import { nanoid } from '@/lib/utils'
 import { Chat } from '@/components/chat'
+import {Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger} from "@/components/ui/sheet";
 
 export default function IndexPage() {
   const id = nanoid()
 
-  return <Chat id={id} />
+  return <>
+    <Sheet>
+      <SheetTrigger>Open</SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Are you sure absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your account
+            and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+    <Chat id={id} />
+  </>
+
 }
