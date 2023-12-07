c5d6570b1ffa74a4c7197af6d32e62a21eb6f083
Guillaume <suiffet.g@gmail.com>
2023-12-07 15:30:41 +0100

    test100

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { Logo } from '@/components/logo';
 import { ModeToggle } from '@/components/mode-toggle';
 import { Button } from '@/components/ui/button';
-// import { Drawer } from '@/components/drawer';
+import { Drawer } from '@/components/drawer';
 import { Icon } from '@/components/icon';
 
 const Header: FC = () => {
@@ -11,7 +11,7 @@ const Header: FC = () => {
         <header className="container h-14 sticky top-0 z-50 border-b bg-background/95 backdrop-blur">
             <div className="flex justify-between items-center h-full w-full px-4 md:px-16">
                 <div className="md:hidden">
-                    {/*<Drawer />*/}
+                    <Drawer />
                 </div>
                 <Logo classname="hidden md:flex" />
                 <div className="flex items-center">
